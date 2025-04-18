a8d98fccf67665730810826e36808fc2fea06b20
Your Name <you@example.com>
2025-04-17 21:03:37 -0400

    new try

--- hashmap.c
+++ hashmap.c
@@ -115,7 +115,7 @@ Pair * searchMap(HashMap * map,  char * key) {
 
 Pair * firstMap(HashMap * map) {
     for (long i = 0 ; i < map->capacity ; i++){ // Recorrer en caso de que el primero es null
-        if (map->buckets[i] != NULL){   // Hasta un dato valido
+        if (map->buckets[i] != NULL && map->buckets[i]->key != NULL){   // Hasta un dato valido
             map->current = i; // Current actualizado
             return map->buckets[i]; // Retorna el dato valido y termina el bucle
         }
@@ -125,7 +125,7 @@ Pair * firstMap(HashMap * map) {
 
 Pair * nextMap(HashMap * map) {
     for (long i = map->current + 1 ; i < map->capacity ; i++){ // "i" inicializa despues de current ya que queremos el next
-        if (map->buckets[i] != NULL){ // En caso de que le siguente dato sea null
+        if (map->buckets[i] != NULL && map->buckets[i]->key != NULL){ // En caso de que le siguente dato sea null
             map->current = i; // Current actualizado
             return map->buckets[i]; // Retorna el dato valido y termina el bucle
         }
